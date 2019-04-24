4cec6d0fa515fd536521d8626da0bedbd8badb94
koneckonca <andreiiernda@mail.ru>
2019-04-24 18:54:45 +0300

    Csv => Avro converter clear release

--- 101/3_avroConverters/bootApplica/src/main/java/kozitski/data/converter/Application.java
+++ 101/3_avroConverters/bootApplica/src/main/java/kozitski/data/converter/Application.java
@@ -47,8 +47,6 @@ public class Application implements CommandLineRunner {
     public void run(String... args) {
         applicationExecutor.runApplication();
 
-        System.out.println("___________________");
-
         context.close();
     }
 
